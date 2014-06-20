23f22f0cc47f13ca12e2e9b3e1022f5f1198765c
Andre de Souza <andre.souza@vetta.com.br>
2014-06-20 13:59:47 -0300

    Resolvendo alerta de compilacao

--- src/main/java/com/javacodegeeks/aspectjspring/aspects/GreetingAspect.java
+++ src/main/java/com/javacodegeeks/aspectjspring/aspects/GreetingAspect.java
@@ -1,22 +1,26 @@
 package com.javacodegeeks.aspectjspring.aspects;
 
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+
 @Aspect
 public class GreetingAspect {
 
-	private String message;
+    private String message;
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
 
-	@Around("execution(* com.javacodegeeks.aspectjspring.services.GreetingService.*(..))")
-	public Object advice(ProceedingJoinPoint pjp) throws Throwable {
-		String serviceGreeting = (String) pjp.proceed();
-		return message + " and " + serviceGreeting;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+    @Around("execution(* com.javacodegeeks.aspectjspring.services.GreetingService.*(..))")
+    public Object advice(ProceedingJoinPoint pjp) throws Throwable {
+        String serviceGreeting = (String) pjp.proceed();
+        return message + " <and> " + serviceGreeting;
+    }
 
 }
