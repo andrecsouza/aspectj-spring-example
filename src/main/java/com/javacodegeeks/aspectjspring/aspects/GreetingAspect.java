436ae2c3f230f64af41ba5cebe1c76bc7a01b1e8
Andre de Souza <andre.souza@vetta.com.br>
2014-06-20 13:23:53 -0300

    Initial commit.

---
+++ src/main/java/com/javacodegeeks/aspectjspring/aspects/GreetingAspect.java
@@ -0,0 +1,22 @@
+package com.javacodegeeks.aspectjspring.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class GreetingAspect {
+
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Around("execution(* com.javacodegeeks.aspectjspring.services.GreetingService.*(..))")
+	public Object advice(ProceedingJoinPoint pjp) throws Throwable {
+		String serviceGreeting = (String) pjp.proceed();
+		return message + " and " + serviceGreeting;
+	}
+
+}
---
+++ src/main/java/com/javacodegeeks/aspectjspring/services/GreetingService.java
@@ -0,0 +1,12 @@
+package com.javacodegeeks.aspectjspring.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("greetingService")
+public class GreetingService {
+
+	public String sayHello() {
+		return "Hello from Greeting Service";
+	}
+
+}
